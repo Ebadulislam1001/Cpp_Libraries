8ce64f7192921fb6c994e0fb433c4a5a03359a84
Ebadulislam <ebadulislamfarooqi@gmail.com>
2024-12-12 18:05:05 +0530

    added InputLib.cpp

---
+++ InputLib.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int get_int(string prompt)
+{
+    printf("%s", prompt.c_str());
+    int input;
+    cin >> input;
+    return input;
+}
+double get_double(string prompt)
+{
+    printf("%s", prompt.c_str());
+    double input;
+    cin >> input;
+    return input;
+}
+char get_char(string prompt)
+{
+    printf("%s", prompt.c_str());
+    char input;
+    cin >> input;
+    return input;
+}
+string get_string(string prompt)
+{
+    printf("%s", prompt.c_str());
+    string input;
+    cin >> input;
+    return input;
+}
+vector<int> get_vector_int(string prompt, int size)
+{
+    printf("%s", prompt.c_str());
+    vector<int> input;
+    for (int i = 0; i < size; i++)
+    {
+        int temp;
+        cin >> temp;
+        input.push_back(temp);
+    }
+    return input;
+}
+vector<double> get_vector_double(string prompt, int size)
+{
+    printf("%s", prompt.c_str());
+    vector<double> input;
+    for (int i = 0; i < size; i++)
+    {
+        double temp;
+        cin >> temp;
+        input.push_back(temp);
+    }
+    return input;
+}
+vector<string> get_vector_string(string prompt, int size)
+{
+    printf("%s", prompt.c_str());
+    vector<string> input;
+    for (int i = 0; i < size; i++)
+    {
+        string temp;
+        cin >> temp;
+        input.push_back(temp);
+    }
+    return input;
+}
